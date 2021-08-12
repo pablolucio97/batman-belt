124eb6d96f57fa9f95cd1f45c2fe6d47a164760d
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-08-12 17:47:50 -0300

    getMaxValueFromArray function added

--- src/utils/arrays/getMaxValueFromArray.ts
+++ src/utils/arrays/getMaxValueFromArray.ts
@@ -2,7 +2,7 @@ const myArray = [1, 6 , 7, 5]
 
 //returns the lowest value from an array of numbers
 
-function getMinValueFromArray(myArray: any[]){
+function getMinValueFromArray(myArray: number[]){
     return Math.min.apply(null, myArray)
 }
 
--- src/utils/arrays/getMinValueFromArray.ts
+++ src/utils/arrays/getMinValueFromArray.ts
@@ -3,7 +3,7 @@ const myArray = [1, 6 , 7, 5]
 
 //returns the higher value from an array of numbers
 
-function getMinValueFromArray(myArray: any[]){
+function getMinValueFromArray(myArray: number[]){
     return Math.max.apply(null, myArray)
 }
 
