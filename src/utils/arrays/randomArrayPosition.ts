ba15075fe15c986569ad0c2352e78861d9c0151b
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-08-12 17:23:33 -0300

    getMinValueFromArray function added

--- src/utils/arrays/randomArrayPosition.ts
+++
@@ -1,8 +0,0 @@
-const myArray = [1, 6 , 7, 5]
-
-//returns a random element from an array
-
-function randomArrayPosition(myArray: any[]){
-    return Math.floor(Math.random() * myArray.length)
-}
-console.log(randomArrayPosition(myArray))
--- src/utils/arrays/shuffleArrays.ts
+++ src/utils/arrays/shuffleArrays.ts
@@ -1,5 +1,7 @@
 const myArray = [1, 6 , 7, 5]
 
+//return a shuffled array
+
 function shuffleArray(myArray: any[]){
     return myArray.sort(() => Math.random() - 0.5)
 }
